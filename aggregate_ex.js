4fcc6132460df94cb13f55a75d879fbd7f1dafbb
Doug Wright <mrdougwright@gmail.com>
2012-12-02 22:42:53 -0800

    hw5

--- aggregate_ex.js
+++ aggregate_ex.js
@@ -8,6 +8,7 @@ db.products.aggregate([
 	}
 ])
 
+//aggregate $sum
 db.products.aggregate([ 
 	{$group: 
 		{
@@ -17,15 +18,8 @@ db.products.aggregate([
 	}
 ])
 
+
 db.zips.aggregate([
-  {"$group":
-   { "_id":{"stateD":"$state"},
-   "population":{"$sum":"$pop"}  
-   }
-  }
-
-
-  db.zips.aggregate([
   {"$group":
    {_id:"$state",
    "population":{"$sum":"$pop"}  
@@ -33,6 +27,7 @@ db.zips.aggregate([
   }
 ])
 
+// $avg
 db.products.aggregate([
 	{$group:
 		{
@@ -43,3 +38,180 @@ db.products.aggregate([
 		}
 	}
 ])
+
+db.zips.aggregate([
+	{$group:
+		{
+			_id:"$state",
+			"average_pop":{$avg:"$pop"}
+		}}
+])
+
+// $addToSet
+db.zips.aggregate([
+	{$group:
+		{
+			_id:"$city",
+			"postal_codes":{$addToSet:"$_id"}
+		}
+	}
+])
+
+// $push
+db.products.aggregate([
+	{$group:
+		{
+			_id:{
+				"maker":"$manufacturer"
+			},
+			categories:{$push:"$category"}
+		}
+	}
+])
+
+// $max and $min
+db.zips.aggregate([
+	{$group:
+		{
+			_id:"$_id",
+			pop:{$max:"$pop"}
+		}}
+])
+
+// $group stages
+db.grades.aggregate([
+	{$group:{
+		_id:{class_id:"$class_id", student_id:"$student_id"},
+		'average':{"$avg":"$score"}
+		}
+	},
+	{$group:{_id:"$_id.class_id", 'average':{"$avg":"$average"}}}
+])
+
+// $project
+db.zips.aggregate([
+	{$project:
+		{
+			_id:0,
+			"city":{$toLower:"$city"},
+			"pop":"$pop",
+			"state":"$state",
+			"zip":"$_id"
+		}
+	}
+])  //or:
+db.zips.aggregate([
+	{$project:
+		{
+			_id:0,
+			"city":{"$toLower":"$city"},
+			pop:1,
+			state:1,
+			"zip":"$_id"
+		}
+	}
+])
+
+// $match
+db.zips.aggregate([
+	{$match:
+		{
+			state:"NY"
+		}
+	},
+	{$group:
+		{
+			_id: "$city",
+			population: {$sum:"$pop"},
+			zip_codes: {$addToSet: "$_id"}
+		}
+	},
+	{$project:
+		{
+			_id:0,
+			city: "$_id",
+			population: "$population", //or population:1
+			zip_codes:1
+		}
+	}
+])
+
+db.zips.aggregate([
+	{$match:
+		{
+			pop:{$gt:100000}
+		}
+	}
+])
+
+// $sort
+db.zips.aggregate([
+	{$sort:
+		{
+			state:1,
+			city:1
+		}
+	}
+])
+
+// using $match, $group, $project, $sort, $skip, $limit
+db.zips.aggregate([
+	{$match:
+		{
+			state:"NY"
+		}
+	},
+	{$group:
+		{
+			_id:"$city",
+			population: {$sum:"$pop"}
+		}
+	},
+	{$project:
+		{
+			_id:0,
+			city:"$_id",
+			population:1,
+		}
+	},
+	{$sort:
+		{
+			population:-1
+		}
+	},
+	{$skip: 10},
+	{$limit: 5}
+])
+
+// get the population of every city in every state
+db.zips.aggregate([
+	{$group:
+		{
+			_id: {state:"$state", city:"$city"},
+			population: {$sum:"$pop"},
+		}
+	},
+	//sort by state, population
+	{$sort:
+		{"_id.state":1, "population":-1}
+	},
+	// group by state, get first item in each group
+	{$group:
+		{
+			_id:"$_id.state",
+			city: {$first: "$_id.city"},
+			population: {$first:"$population"}
+		}
+	},
+	{$sort: {population:1}}
+])
+
+// $unwind to unjoin data
+// {a:1,b:[A1,B1]} would become:
+// {a:1,b:A1}
+// {a:1,b:B1}
+// $push is like the opposite to $unwind
+
+
+
+
---
+++ hw5.js
@@ -0,0 +1,69 @@
+//hw 5.1
+db.posts.aggregate([
+	{"$unwind":"$comments"},
+	{$group:
+		{
+			_id:"$comments.author",
+			num_comments:{$sum:1}
+		}
+	},
+	{$sort:
+		{
+			num_comments:1
+		}
+	}
+])
+
+// hw 5.2 - calculate ave population of cities in CA 
+//and NY when over 25,000
+db.zips.aggregate([
+	{$match:
+		{
+			state:{$in:["NY","CA"]}
+		}
+	},
+	{$group:
+		{
+			_id:{state:"$state",city:"$city"},
+			"sum":{"$sum":"$pop"}
+		}
+	},
+	{$match:
+		{
+			sum:{"$gt":25000}
+		}
+	},
+	{$group:
+		{
+			_id:null,
+			"average_pop":{$avg:"$sum"}
+		}
+	}
+])
+
+// hw 5.3
+db.grades.aggregate([
+	{$unwind:"$scores"},
+	{$match:
+		{
+			type:{$ne:"$quiz"}
+		}
+	},
+	{$group:
+		{
+			_id:"$class_id",
+			"avg_score":{$avg:"$scores.score"}
+		}
+	},
+	{$group:
+		{
+			_id:"$_id",
+			"avg_for_class":{$avg:"$avg_score"}
+		}
+	},
+	{$sort:{"avg_for_class":1}}
+])
+
+
+
+
