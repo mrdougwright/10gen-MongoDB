7c8288f3e732872ed3f97c22d4be5e7e3dd39476
Doug Wright <mrdougwright@gmail.com>
2012-12-02 23:15:13 -0800

    hw5 done

--- hw5.js
+++ hw5.js
@@ -64,6 +64,25 @@ db.grades.aggregate([
 	{$sort:{"avg_for_class":1}}
 ])
 
-
-
+// hw 5.4
+db.zips.aggregate([
+	{$project:
+		{
+			first_char: {$substr: ["$city",0,1]},
+			pop: "$pop",
+			zipcode: "$_id"
+		}
+	},
+	{$match:
+		{
+			first_char:{$in:["0","1","2","3","4","5","6","7","8","9"]}	
+		}
+	},
+	{$group:
+		{
+			_id:0,
+			"total":{$sum:"$pop"},
+		}
+	}
+])
 
